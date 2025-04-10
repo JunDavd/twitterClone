48e8dde583c3fb95ada1a725f87e1ca70edc24d8
Juan Ruiz <jdruiz008@gmail.com>
2025-04-10 18:10:42 -0500

    refactored showTweetsController.js

--- show-tweets/showTweetsModel.js
+++ show-tweets/showTweetsModel.js
@@ -1,5 +1,8 @@
 // Modelo --> obtencion de datos que necesita nuestra aplicacion
-export async function getTweets(){
-  const tweets = await fetch('http://localhost:8000/api/tweets')
+export function getTweets(){
+  return fetch('http://localhost:8000/api/tweets')
+  .then((response) => {
+    return response.json()
+  })
 }
 
